e8a49b9893a3d708bfd937fa2835c45c87f5915d
xecnorex <muhdsayyidizzuddin@gmail.com>
2025-12-13 20:07:18 +0800

    tah

---
+++ calculator.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int main () 
+{
+  float input_one;
+  float input_two;
+  float ans;
+  char operator;
+  char anss;
+
+beginning:
+  printf("Enter your input: \n");
+  scanf("%f", &input_one);
+
+sec_input:
+  printf("Enter your second input: \n");
+  scanf("%f", &input_two);
+
+  printf("Please choose your operator (+, -, *, /): \n");
+  scanf(" %c", &operator);
+
+  switch (operator)
+  {
+    case '+':
+      ans = input_one + input_two;
+      printf("%.2f\n", ans);
+      break;
+
+    case '-':
+      ans = input_one - input_two;
+      printf("%.2f\n", ans);
+      break;
+
+    case '*':
+      ans = input_one * input_two;
+      printf("%.2f\n", ans);
+      break;
+    
+    case '/':
+      if (input_two != 0)
+      {
+        ans = input_one / input_two;
+        printf("%.2f\n", ans);
+        break;
+      }
+      else {
+        printf("The calculation is invalid. Please do not divide by 0.\n");
+        goto sec_input;
+      }
+
+      break;
+
+    default:
+      printf("Invalid operator. Please try again\n");
+
+      break;
+  }
+
+  printf("Try again? y/n \n");
+  scanf(" %c", &anss);
+
+  if (anss == 'y' || anss == 'Y') {
+    goto beginning;
+  }
+
+  return 0;
+}
